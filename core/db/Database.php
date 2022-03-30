243b91f289bc6bac5aa467a710dd67099f4859ab
Daniel <S0us4.dan@gmail.com>
2022-03-30 14:43:15 -0300

    Adicionando pastas

--- core/db/Database.php
+++ core/db/Database.php
@@ -7,7 +7,6 @@ use PDOException;
 
 class Database
 {
-
     /**
      * Essa é a constante que define o nome da conexão com o banco de dados
      */
@@ -31,7 +30,7 @@ class Database
     /**
      * Atributo que vai receber a tabela
      */
-    private $tabela;
+    private $table;
 
     /**
      * Atributo que define a minha conexão com o banco de dados
@@ -62,7 +61,61 @@ class Database
         }
     }
 
-//prepara os dados da query (nível médio de dificuldade)
+    public function insert($values)
+    {
+        //passa os dados da query
+        $fields = array_keys($values);
+        $binds = array_pad([], count($fields), '?');
+
+        //monta a query
+        $query = 'INSERT INTO ' . $this->table . '(' . implode(',', $fields) . ') VALUES ( ' . implode(',', $binds);
+
+        //Executa query 
+        $this->execute($query, array_values($values));
+
+        //retorna o último id inserido
+        return $this->connection->lastInsertId();
+    }
+
+
+    public function select($where = NULL, $order = NULL, $limit = NULL, $fields = '*')
+    {
+        //preparar os dados da query 
+        $where = strlen($where) ? 'WHERE ' . $where : '';
+        $order = strlen($order) ? 'ORDER BY ' . $order : '';
+        $limit = strlen($limit) ? 'LIMIT ' . $limit : '';
+
+        //Monta a query
+        $query = 'SELECT' . $fields . ' FROM ' . $this->table . ' ' . $where . ' ' . $order . ' ' . $limit;
+
+        //executa a query
+        $this->execute($query);
+    }
+    
+
+    public function update($where, $values)
+    {
+        //passa os dados da query
+        $fields = array_keys($values);
+
+        //monta a query
+        $query = 'UPDATE' . $this->table . 'SET' . implode('=?', $fields) . '=? WHERE' . $where;
+    }
+
+
+    public function delete($where)
+    {
+        //montar a query
+        $query =  'DELETE FROM' . $this->table . ' WHERE ' . $where;
+
+        //executar a query
+        $this->execute($query);
+
+        //retorna sucesso 
+        return true;
+    }
+
+//prepara os dados da query
 //Monta a query a ser executada
 //executa a query(nível de dificuldade fácil)
 }
